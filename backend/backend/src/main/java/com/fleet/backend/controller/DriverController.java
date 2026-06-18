5965f665dd664daf8e21d3d2fb6a047375fbefbf
Pablo Henrique Gomes <Pablohenriquerst7@gmail.com>
2026-06-18 17:01:07 -0300

    CRUD completo de Driver implementado

--- backend/backend/src/main/java/com/fleet/backend/controller/DriverController.java
+++ backend/backend/src/main/java/com/fleet/backend/controller/DriverController.java
@@ -2,11 +2,11 @@ package com.fleet.backend.controller;
 
 import com.fleet.backend.entity.Driver;
 import com.fleet.backend.repository.DriverRepository;
+import org.aspectj.apache.bcel.Repository;
 import org.springframework.web.bind.annotation.*;
 import com.fleet.backend.entity.Vehicle;
 import org.springframework.http.ResponseEntity;
 import java.util.Optional;
-
 import java.util.List;
 
 @RestController
@@ -40,4 +40,53 @@ public class DriverController {
 
         return ResponseEntity.notFound().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Driver> buscarPorId(@PathVariable Long id) {
+
+        Optional<Driver> driver = repository.findById(id);
+
+        if (driver.isPresent()) {
+            return ResponseEntity.ok(driver.get());
+
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Driver> atualizar(
+            @PathVariable Long id,
+            @RequestBody Driver driver
+    ){
+
+        Optional<Driver> driverExistente = repository.findById(id);
+
+        if (driverExistente.isPresent()) {
+
+            Driver driverAtualizado = driverExistente.get();
+
+            driverAtualizado.setName(driver.getName());
+            driverAtualizado.setLicenseNumber(driver.getLicenseNumber());
+
+            return ResponseEntity.ok(
+                    repository.save(driverAtualizado)
+            );
+
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+
+        if (repository.existsById(id)) {
+
+            repository.deleteById(id);
+
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
